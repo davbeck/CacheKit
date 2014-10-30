d43881f0070a2e12618e4631da9fe9dd7894163b
David Beck <code@thinkultimate.com>
2014-10-30 13:42:29 -0700

    Added Pods directly to gitignore

--- Example/Pods/Headers/Public/CacheKit/CKSQLiteCache.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/CKSQLiteCache.h
--- Example/Pods/Target Support Files/Pods-CacheKit/Pods-CacheKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CacheKit : NSObject
-@end
-@implementation PodsDummy_Pods_CacheKit
-@end
